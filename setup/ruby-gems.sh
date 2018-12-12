fdc5a0136af420fa17b5ffad101d0efbe646bfb5
Robert Audi <robert.audii@gmail.com>
2018-12-12 13:11:49 +0100

    Add a small description to the 'extended_bundler-errors' plugin

--- setup/ruby-gems.sh
+++ setup/ruby-gems.sh
@@ -7,6 +7,11 @@ gem install yard --env-shebang --no-document
 # Manages an application's dependencies
 #   https://bundler.io/
 gem install bundler --env-shebang --no-document
+
+# A Bundler plugin that makes gem installation errors more
+# actionable, educative, and all around easier to understand
+#   https://github.com/jules2689/extended_bundler-errors
+#   https://rubygems.org/gems/extended_bundler-errors
 bundler plugin install extended_bundler-errors
 
 if type install-default-gems; then
