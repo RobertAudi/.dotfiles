513c980565b22edc03e0a7ac7ecde4ecf77c3e4a
Robert Audi <robert.audii@gmail.com>
2018-12-30 11:39:25 +0100

    Add the `forget-blob` git command

--- git/.zsh/plugins/zplugin.d/git.zplugin.zsh
+++ git/.zsh/plugins/zplugin.d/git.zplugin.zsh
@@ -4,5 +4,8 @@ zplugin light paulirish/git-open
 zplugin ice as"command" pick"git-foresta"
 zplugin light takaaki-kasai/git-foresta
 
+zplugin ice as"command" cp"git-forget-blob.sh -> git-forget-blob" pick"git-forget-blob"
+zplugin light nachoparker/git-forget-blob
+
 # local plugins
 zplugin load _local/git
