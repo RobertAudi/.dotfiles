87d2d2af029d03ea4b9426e4ccbfd7bf43c22dad
Robert Audi <robert.audii@gmail.com>
2019-01-01 16:12:58 +0100

    Do not expand zsh aliases when compiling function files

--- zsh/.zsh/011_run-help.zsh
+++ zsh/.zsh/011_run-help.zsh
@@ -8,16 +8,3 @@ autoload -Uz run-help-git
 autoload -Uz run-help-openssl
 autoload -Uz run-help-sudo
 autoload -Uz run-help
-autoload +X run-help
-
-function help() {
-  if (( $# == 0 )); then
-    run-help
-  elif is-function "run-help-$1"; then
-    local cmd="run-help-$1"
-    shift
-    $cmd $@
-  else
-    run-help $@
-  fi
-}
