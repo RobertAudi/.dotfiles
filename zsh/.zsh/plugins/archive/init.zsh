b000e886ca82ac15f15fde88e1e7265f8fa9ac6b
Robert Audi <robert.audii@gmail.com>
2019-01-24 15:58:26 +0100

    Fix the archive zsh plugin

--- zsh/.zsh/plugins/archive/init.zsh
+++ zsh/.zsh/plugins/archive/init.zsh
@@ -1,5 +1,9 @@
 fpath=("$ZSH_HOME/plugins/archive/functions" $fpath)
-autoload -Uz $ZSH_HOME/plugins/archive/functions/*~(*~|*.zwc)(-N.:t)
+function {
+  emulate -L zsh
+  setopt extended_glob
+  autoload -Uz $ZSH_HOME/plugins/archive/functions/*~(*~|*.zwc)(-N.:t)
+}
 
 if type atool >/dev/null; then
   alias xx="atool -x"
